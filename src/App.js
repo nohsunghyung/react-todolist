d5db1e43deb3d071fc4a8b6d640ada691c6ecee8
DESKTOP-S6IR4FG\nolu8 <noh8085@gmail.com>
2021-01-06 23:14:14 +0900

    프로젝트 init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="wrapper">
+          <header>TodoList</header>
+          <div className="content">
+            <div className="insert">
+              <form>
+                <input type="text" placeholder="Todo를 입력해주세요" />
+                <button className="btn btn-add">등록</button>
+              </form>
+            </div>
+            <ul className="todo-list">
+              <li className="list-item active">
+                <div className="text">todoList</div>
+                <div className="checked">
+                  <input type="checkbox" id="chk1" />
+                </div>
+                <div className="button">
+                  <button type="button" className="btn btn-modify">
+                    수정
+                  </button>
+                  <button type="button" className="btn btn-delete">
+                    삭제
+                  </button>
+                </div>
+              </li>
+              <li className="list-item">
+                <div className="text">todoList</div>
+                {/* 수정 */}
+                {/* <div>
+                  <input
+                    type="text"
+                    placeholder="todo를 적어주세요"
+                  />
+                </div> */}
+                <div className="checked">
+                  <input type="checkbox" id="chk1" />
+                </div>
+                <div className="button">
+                  <button type="button" className="btn btn-modify">
+                    수정
+                  </button>
+                  <button type="button" className="btn btn-delete">
+                    삭제
+                  </button>
+                </div>
+                {/* 수정눌렀을경우 */}
+                {/* <div className="button">
+                  <button
+                    type="button"
+                    className="btn btn-ok"
+                  >
+                    확인
+                    </button>
+                  <button
+                    type="button"
+                    className="btn btn-cancel"
+                  >
+                    취소
+                    </button>
+                </div> */}
+                {/* 수정눌렀을경우 */}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
